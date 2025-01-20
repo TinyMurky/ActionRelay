9041f671c57a668024e55274c3cc3b12cb722597
Tiny_Murky <murky0830@gmail.com>
2025-01-20 16:52:07 +0800

    Move main logic to post logic
    Since gathering data need to be after all jobs and steps complete, it
    must need to be execute under post
    
    haha
    
    a
    
    a
    
    a

--- rollup.config.ts
+++ rollup.config.ts
@@ -5,17 +5,17 @@ import nodeResolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
 import json from '@rollup/plugin-json'
 
-const config = {
-  input: 'src/index.ts',
-  output: {
-    esModule: true,
-    // file: 'dist/index.js',
-    dir: 'dist',
-    format: 'es',
-    sourcemap: true
-  },
-  plugins: [nodeResolve(), commonjs(), json(), typescript()]
-  // external: ['@mermaid-js/mermaid-cli']
-}
+const config = [
+  {
+    input: ['src/index.ts', 'src/post.ts'],
+    output: {
+      esModule: true,
+      dir: 'dist',
+      format: 'es',
+      sourcemap: true
+    },
+    plugins: [nodeResolve(), commonjs(), json(), typescript()]
+  }
+]
 
 export default config
